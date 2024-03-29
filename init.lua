37ca8f1f5727fed678dbf465db508a92e766171e
Hubbard Harvey <hcubedcoder@gmail.com>
2024-03-29 07:39:39 -0400

    Add tab options

--- init.lua
+++ init.lua
@@ -139,6 +139,15 @@ vim.opt.timeoutlen = 300
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
+--Tab settings
+vim.opt.expandtab = false
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+-- Indenting
+vim.opt.smartindent = true
+
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
